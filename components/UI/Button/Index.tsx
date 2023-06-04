6b8b1c9dfc4d566fc044a32bb0657768d625dc21
llite22 <llite2@yandex.ru>
2023-06-04 23:19:18 +0300

    test

--- components/UI/Button/Index.tsx
+++ components/UI/Button/Index.tsx
@@ -4,9 +4,8 @@ import styles from './Button.module.scss'
 const Button:FC = () => {
   return (
     <div className={styles.button}>
-         <button className="text-white ">Search</button>
+         <button className="text-white">Search</button>
     </div>
-   
   )
 }
 export default Button
