e731e08cc628dec8428e6c4e2f03d712836ec30c
llite22 <llite2@yandex.ru>
2023-06-04 21:16:58 +0300

    add reviews

--- components/UI/MovieCard/Index.tsx
+++ components/UI/MovieCard/Index.tsx
@@ -5,6 +5,10 @@ import Image from "next/image";
 const MovieCard: FC = () => {
   return (
     <div className={styles.movieCard}>
+        <div className={styles.reviews}>
+            <Image src='/image/star.svg' width={16} height={16} alt="star"/>
+            <span>6.8</span>
+        </div>
       <Image
         className="rounded-xl mt-2"
         src="/image/widow.jpg"
