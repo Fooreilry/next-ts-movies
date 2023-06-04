283cd1088f573c3ef592d33ce98e56992484e655
llite22 <llite2@yandex.ru>
2023-06-04 17:01:28 +0300

    switch готов

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Input from "@/components/UI/Input/Index"
 import Button from '../components/UI/Button/Index';
 import Heading from "@/components/UI/Heading/Index";
+import Switch from "@/components/UI/Switch/Index";
 
 
 export default function Home() {
   return (
     <main>
-   <Heading/>
+   <Switch/>
     </main>
   )
 }
