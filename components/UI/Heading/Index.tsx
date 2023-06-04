01ec888f2d20da219e989d06fbc54ee5799b8c22
llite22 <llite2@yandex.ru>
2023-06-04 16:15:05 +0300

    ui готовы

---
+++ components/UI/Heading/Index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+import styles from "./Heading.module.scss";
+
+const Heading: FC = () => {
+  return <h1 className={styles.heading}>MaileHereko</h1>;
+};
+export default Heading;
