9871c1ce01070b1ccc2fbcbaf6bc512c98f0111d
llite22 <llite2@yandex.ru>
2023-06-05 13:21:55 +0300

    buttonLink

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Button from '../components/UI/Button/Index';
 import Heading from "@/components/UI/Heading/Index";
 import Switch from "@/components/UI/Switch/Index";
 import MovieCard from "@/components/UI/MovieCard/Index";
-import Header from "@/components/Header/Index";
 import Skeleton from "@/components/UI/Skeleton/Index";
+import ButtonLink from "@/components/UI/Button/buttonLink";
+
 
 
 export default function Home() {
   return (
     <main>
-   <Input/>
     </main>
   )
 }
