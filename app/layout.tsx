bd3e417f5d72362c4d3125896031dbc106f9869a
Sergey <serdzopadzo@gmail.com>
2023-06-17 13:38:31 +0300

    fix fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.style}>
         <Navbar/>
         <main className='container mx-auto px-4'>{children}</main>
       </body>
